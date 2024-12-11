1ba60b23a4d7c8edd6d8687367124a6ff3fced3e
elijahnikov <enkv@proton.me>
2024-12-11 23:46:49 +0000

    intiail work on avatar

--- src/app/input/page.tsx
+++ src/app/input/page.tsx
@@ -1,4 +1,10 @@
 import ThemeToggle from "@/components/common/theme-toggle";
+import {
+	Avatar,
+	AvatarFallback,
+	AvatarImage,
+	AvatarRoot,
+} from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
@@ -9,6 +15,19 @@ export default function InputPage() {
 		<div className="min-h-screen min-w-screen p-16 flex flex-col items-center justify-center">
 			<ThemeToggle />
 			<Card>
+				<div className="flex items-center gap-2">
+					<Avatar
+						src={
+							"https://images.unsplash.com/photo-1560800452-f2d475982b96?auto=format&fit=crop&w=250&h=250"
+						}
+					/>
+					<div className="flex flex-col leading-none">
+						<p className="font-medium">John Doe </p>
+						<p className="text-sm text-neutral-500 dark:text-neutral-400">
+							john.doe@example.com
+						</p>
+					</div>
+				</div>
 				<div className="min-w-[400px] flex flex-col gap-2">
 					<p className="font-medium">Email</p>
 					<Input placeholder="Enter your email" />
---
+++ src/components/ui/avatar.tsx
@@ -0,0 +1,172 @@
+"use client";
+
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+import { type VariantProps, cva } from "class-variance-authority";
+import { UserIcon } from "lucide-react";
+import * as React from "react";
+import { cn } from "../utils/cn";
+
+export const rootClasses =
+	"h-10 min-w-10 relative inline-flex aspect-square shrink-0 items-center antialiased";
+
+export const avatarVariants = cva(rootClasses, {
+	variants: {
+		size: {
+			xxs: "h-5 min-w-5 text-xs",
+			xs: "h-6 min-w-6 text-sm",
+			sm: "h-8 min-w-8 text-base",
+			md: "h-10 min-w-10 text-lg",
+			lg: "h-12 min-w-12 text-xl",
+			xl: "h-14 min-w-14 text-2xl",
+			"2xl": "h-16 min-w-16 text-3xl",
+		},
+	},
+	defaultVariants: {
+		size: "md",
+	},
+});
+
+export const fallbackVariants = cva("", {
+	variants: {
+		size: {
+			xxs: "size-3",
+			xs: "size-4",
+			sm: "size-5",
+			md: "size-6",
+			lg: "size-7",
+			xl: "size-8",
+			"2xl": "size-10",
+		},
+	},
+	defaultVariants: {
+		size: "md",
+	},
+});
+
+export type AvatarElement =
+	| React.ElementRef<typeof AvatarPrimitive.Image>
+	| HTMLSpanElement;
+
+type AvatarVariantProps = VariantProps<typeof avatarVariants> &
+	VariantProps<typeof fallbackVariants>;
+
+export type AvatarProps = React.ComponentPropsWithoutRef<
+	typeof AvatarPrimitive.Image
+> &
+	AvatarVariantProps & {
+		delayMs?: number;
+		initials?: string;
+	};
+
+const AvatarRoot = React.forwardRef<
+	React.ElementRef<typeof AvatarPrimitive.Root>,
+	React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>(({ className, ...props }, ref) => (
+	<AvatarPrimitive.Root
+		ref={ref}
+		className={cn(rootClasses, className)}
+		{...props}
+	/>
+));
+AvatarRoot.displayName = AvatarPrimitive.Root.displayName;
+
+const AvatarImage = React.forwardRef<
+	React.ElementRef<typeof AvatarPrimitive.Image>,
+	React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>(({ className, ...props }, ref) => (
+	<AvatarPrimitive.Image
+		ref={ref}
+		className={cn(
+			"aspect-square border shadow-overlay  w-full grow rounded-full object-cover object-center",
+			className,
+		)}
+		{...props}
+	/>
+));
+AvatarImage.displayName = AvatarPrimitive.Image.displayName;
+
+const AvatarFallback = React.forwardRef<
+	React.ElementRef<typeof AvatarPrimitive.Fallback>,
+	React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>(({ className, ...props }, ref) => (
+	<AvatarPrimitive.Fallback
+		ref={ref}
+		className={cn(
+			"flex h-full bg-carbon-200 text-neutral-500 dark:text-neutral-400 w-full border shadow-overlay dark:shadow-xs-dark dark:bg-carbon-dark-500 dark:border-neutral-600 items-center justify-center rounded-full",
+			className,
+		)}
+		{...props}
+	/>
+));
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
+
+const Avatar = React.forwardRef<AvatarElement, AvatarProps>(
+	(
+		{ size, className, initials, children, alt, style, delayMs, src, ...props },
+		ref,
+	) => {
+		return (
+			<AvatarRoot
+				className={cn(
+					avatarVariants({ size }),
+					!src && !initials && children ? "aspect-auto w-auto" : "",
+				)}
+			>
+				{src && (
+					<AvatarImage
+						ref={ref as React.RefObject<HTMLImageElement>}
+						alt={alt}
+						className={className}
+						src={src}
+						style={style}
+						{...props}
+					/>
+				)}
+
+				{children && (
+					<AvatarFallback
+						ref={ref}
+						aria-label={alt}
+						delayMs={delayMs}
+						asChild={React.isValidElement(children)}
+						className={className}
+						style={style}
+					>
+						{children}
+					</AvatarFallback>
+				)}
+
+				{!children && initials && (
+					<AvatarPrimitive.Fallback
+						ref={ref}
+						aria-label={alt}
+						className={cn(
+							"flex aspect-square grow select-none items-center justify-center rounded-full uppercase text-white",
+							className,
+						)}
+						style={style}
+						{...props}
+					>
+						{initials}
+					</AvatarPrimitive.Fallback>
+				)}
+
+				{!children && !src && !initials && (
+					<AvatarFallback
+						ref={ref}
+						aria-label={alt}
+						className={cn(className)}
+						role="img"
+						style={style}
+						{...props}
+					>
+						<UserIcon className={fallbackVariants({ size })} />
+					</AvatarFallback>
+				)}
+			</AvatarRoot>
+		);
+	},
+);
+Avatar.displayName = "Avatar";
+
+export { AvatarRoot, AvatarImage, AvatarFallback, Avatar };
